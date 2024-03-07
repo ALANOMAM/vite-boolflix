efcc4fef5251d89f721fe52d6a855497663046f6
alan_omam <omamalan@hotmail.it>
2024-03-07 11:20:19 +0100

    prima impostazione dell'header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+   name:'AppHeader',
+
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="header">
+        <span>BOOLFLIX</span>
+
+         <div class="ricerca">
+        <input type="text" placeholder="Scivi nome film">
+        <button>cerca</button>
+        </div>
+    
+    </div>
+</template>
+
+<style>
+
+
+</style>
