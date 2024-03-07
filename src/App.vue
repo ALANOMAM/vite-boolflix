19e2ab46f3eb2d48959d4031ed8ae16200ccc26e
alan_omam <omamalan@hotmail.it>
2024-03-07 11:38:02 +0100

    creato file "store.js" e importato nei vari componenti

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 
 <script>
+import axios from 'axios'
+import {store} from './store'
 import AppHeader from './components/AppHeader.vue'
 export default{
   
   components:{
     AppHeader,
   },
-  
+
   data(){
     return{
-
+        store,
     }
   }
 }
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,12 @@
 <script>
+import axios from 'axios'
+import {store} from '../store'
 export default{
    name:'AppHeader',
 
   data(){
     return{
-      
+      store,
     }
   }
 }
