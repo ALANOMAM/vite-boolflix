9fa4f8433c2d2f7f1555254024c7613f1b3251f3
alan_omam <omamalan@hotmail.it>
2024-03-07 12:23:43 +0100

    salvato oggetti legati ad un film specifico nel file store.js

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,18 @@ export default{
     return{
         store,
     }
-  }
+  },
+
+  created(){
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=756ab663684b23166a50d98c73cd1472&query=ritorno+al+futuro')
+    .then(res=> {
+   
+        //console.log(res.data.results[0].poster_path)
+        // salvo i vari oggetti legati al film nel mio array "movies" nel file "store.js"
+        this.store.movies = res.data.results
+       
+    })
+  },
 }
 </script>
 
