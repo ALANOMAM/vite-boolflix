e9c3bd1a7a784504aa366d40bca70f946d3d10b4
alan_omam <omamalan@hotmail.it>
2024-03-07 13:04:37 +0100

    fatto uno scheletro del layout e iniziato a riempire i diversi items

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -12,11 +12,41 @@ export default{
 </script>
 
 <template>
+<div class="layout">
+    <div v-for="movie in store.movies" class="item-box">
+<div  class="item">
+    <span>qui ci andrà l'immagine</span>
+    <span>{{movie.title }}</span>
+    <span>{{movie.original_title }}</span>
+    <span>{{movie.original_language }}</span>
+    <span>{{movie.vote_average }}</span>
+    </div>
+ </div>
+</div>
 
- 
 </template>
 
 <style>
+.layout{
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.item-box{
+    width: calc(100% /2 );
+    display: flex;
+    border: 1px solid blue;
+    justify-content: center;
+    /*padding: 20px;*/
+}
+
+.item{
+  border: 1px red solid;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  align-items: center;
+}
 
 
 </style>
